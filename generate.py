a36445c84461948a07ba4b2907f7cff4bef4cc47
Egor Nosach <beregdsk@gmail.com>
2023-10-15 02:29:41 +0300

    Refactored code

--- generate.py
+++ generate.py
@@ -2,109 +2,130 @@ import re
 from sympy import Matrix, zeros
 from sympy.polys.matrices import DomainMatrix
 from itertools import groupby
+from collections import deque
 
 import reference
 
-HYBRIDIZATION = False
+class ReactionGenerator:
+    def __init__(self, species_to_solve, ref, hyb=False):
+        self.species_to_solve = species_to_solve
+        self.hybridization = hyb
+        
+        self.re_elems = re.compile(r'([A-Z]+?)(?=\d+)?')
+        self.re_with_counts = re.compile(r'([A-Z]+?)(\d+)?')
+        elements = sorted(set([e for r in reference.reference.keys() 
+                          for e in self.re_elems.findall(r.split('_')[-2]) 
+                          if not (self.hybridization and 'C' in e)]))
+        self.elem_num = {el:i for i, el in enumerate(elements)}
+        
+        if hyb: self.elem_num.update({f'C{i}':len(self.elem_num)+i for i in range(4)})
 
-species_to_solve = ['C18H14', (0,0,16,2)]
+        self.num_elem = dict(map(reversed, self.elem_num.items()))
 
-re_elems = re.compile(r'([A-Z]+?)(?=\d+)?')
-re_with_counts = re.compile(r'([A-Z]+?)(\d+)?')
+        self.dim = len(self.elem_num)
+        
+        self.basis = [self.str_to_vec(*species_to_solve)] + \
+            [self.str_to_vec(r.split('_')[-2], 
+                reference.self.hybridization_1[r] if self.hybridization else None) 
+                for r in reference.reference.keys()]
 
-elem_num = {el:i for i, el in enumerate(set([e for r in reference.reference.keys() for e in re_elems.findall(r.split('_')[-2]) if not (HYBRIDIZATION and 'C' in e)]))}
-if HYBRIDIZATION: 
-	l = len(elem_num)
-	elem_num.update({f'C{i}':l+i for i in range(4)})
+    def str_to_vec(self, s, hyb=None):
+        r_vec = zeros(self.dim, 1)
+        for el, n in self.re_with_counts.findall(s):
+            if self.hybridization and el=='C':
+                for i, c in enumerate(hyb):
+                    r_vec[self.elem_num[f'C{i}']] = int(c)
+            else:
+                r_vec[self.elem_num[el]] = int(n) if n else 1
+        return r_vec
 
-dim = len(elem_num)
-num_elem = dict(map(reversed, elem_num.items()))
+    @staticmethod
+    def minimal_nullspace(A):
+        n = A.to_field().nullspace().to_Matrix()
+        found = n.shape[0]>=1
 
-def str_to_vec(s, hyb=None):
-	r_vec = zeros(dim, 1)
-	for el, n in re_with_counts.findall(s):
-		if HYBRIDIZATION and el=='C':
-			for i, c in enumerate(hyb):
-				r_vec[elem_num[f'C{i}']] = int(c)
-		else:
-			r_vec[elem_num[el]] = int(n) if n else 1
-	return r_vec
+        return (n if n.shape[0]==1 and not 0 in n else None), found
 
-basis = [str_to_vec(*species_to_solve)] + [str_to_vec(r.split('_')[-2], reference.hybridization_1[r] if HYBRIDIZATION else None) for r in reference.reference.keys()]
-basis_l = len(basis)
+    def generate(self):
+        S = []
+        vec_nums = deque([0, 1])
+        i = len(vec_nums)
+        while len(vec_nums) > 1:
+            A = DomainMatrix.from_Matrix(Matrix.hstack(*([self.basis[k] for k in vec_nums])))
+            n, found = self.minimal_nullspace(A)
 
-def minimal_nullspace(A):
-	n = A.to_field().nullspace().to_Matrix()
-	found = n.shape[0]>=1
+            if found:
+                if n is not None:
+                    S.append([vec_nums.copy(), n/n[0]])
+                vec_nums.pop()
 
-	return (n if n.shape[0]==1 and not 0 in n else None), found
+            if i < len(self.basis): 
+                vec_nums.append(i)
+            else:
+                while vec_nums[-1]>=len(self.basis) and len(vec_nums)>1:
+                    vec_nums.pop()
+                i = vec_nums.pop() + 1
+                if i < len(self.basis):
+                    vec_nums.append(i)
 
-S = []
-vec_nums = [0, 1]
-i = 2
-while len(vec_nums) > 1:
-	A = DomainMatrix.from_Matrix(Matrix.hstack(*(basis[k] for k in vec_nums)))
-	n, found = minimal_nullspace(A)
+            i += 1
 
-	if found:
-		if n is not None:
-			S.append([vec_nums.copy(), n/n[0]])
-		vec_nums.pop()
+        return S
 
-	if i < basis_l: 
-		vec_nums.append(i)
-	else:
-		while vec_nums[-1]>=basis_l and len(vec_nums)>1:
-			vec_nums.pop()
-		i = vec_nums.pop() + 1
-		if i < basis_l:
-			vec_nums.append(i)
+    @staticmethod
+    def coef_to_str(c):
+        return str(c) if c!=1 else ''
 
-	i += 1
+    def vector_to_str(self, v):
+        s = ''
 
-def coef_to_str(c):
-	return str(c) if c!=1 else ''
+        if self.hybridization:
+            c_sum = sum(v[n] for (e, n) in self.elem_num.items() if 'C' in e)
+            if c_sum > 0:
+                s += ''.join(f'C{self.coef_to_str(c_sum)}')
 
-def vector_to_str(v):
-	s = ''
+        for i in range(self.dim):
+            if (self.hybridization and 'C' in self.num_elem[i]): continue 
+            if v[i] > 0: s += ''.join(f'{self.num_elem[i]}{self.coef_to_str(v[i])}')
+        return s
 
-	if HYBRIDIZATION:
-		c_sum = sum(v[n] for (e, n) in elem_num.items() if 'C' in e)
-		if c_sum > 0:
-			s += ''.join(f'C{coef_to_str(c_sum)}')
+    def reaction_to_str(self, rxn, cfs):
+        reagents = [[r, c] for (r, c) in zip(rxn, cfs) if c>0]
+        products = [[r, c] for (r, c) in zip(rxn, cfs) if c<0]
 
-	for i in range(dim):
-		if (HYBRIDIZATION and 'C' in num_elem[i]): continue 
-		if v[i] > 0: s += ''.join(f'{num_elem[i]}{coef_to_str(v[i])}')
-	return s
+        s = ''
+        s += '+'.join(
+                '*'.join(
+                    filter(None, [self.coef_to_str(c), self.vector_to_str(self.basis[r])])) 
+                for (r, c) in reagents)
+        s += '->'
+        s += '+'.join(
+                '*'.join(
+                    filter(None, [self.coef_to_str(-c), self.vector_to_str(self.basis[r])])) 
+                for (r, c) in products)
 
-def reaction_to_str(rxn, cfs):
-	reagents = [[r, c] for (r, c) in zip(rxn, cfs) if c>0]
-	products = [[r, c] for (r, c) in zip(rxn, cfs) if c<0]
+        return s
 
-	s = ''
-	s += '+'.join('*'.join(filter(None, [coef_to_str(c), vector_to_str(basis[r])])) for (r, c) in reagents)
-	s += '->'
-	s += '+'.join('*'.join(filter(None, [coef_to_str(-c), vector_to_str(basis[r])])) for (r, c) in products)
-
-	return s
-
-def is_integer_vec(v):
-	for i in range(1,len(v)):
-		if not v[i].is_integer:
-			return False
-	return True
+    @staticmethod
+    def is_integer_vec(v):
+        for i in range(1,len(v)):
+            if not v[i].is_integer:
+                return False
+        return True
 
 def sort_func(rxn):
-	return sum(abs(rxn[1]))+abs(sum(rxn[1]))
+    return sum(abs(rxn[1]))+abs(sum(rxn[1]))
 
-n_rxn = len(S)
-S = list(filter(lambda rxn: is_integer_vec(rxn[1]), S))
-S = sorted(S, key=sort_func)
+if __name__ == '__main__':
+    gen = ReactionGenerator(('C18H14', (0,0,16,2)), reference.hybridization)
 
-layers = [list(g[1]) for g in groupby(S, sort_func)]
+    rxns = gen.generate()
+    int_rxns = list(filter(lambda rxn: gen.is_integer_vec(rxn[1]), rxns))
+    int_rxns = sorted(int_rxns, key=sort_func)
 
-print(f'Reactions found: {n_rxn}, integer: {len(S)}')
-for i in range(len(layers)):
-	print(f'Layer {i}:')
-	print(*(reaction_to_str(*rxn) for rxn in layers[i]), sep='\n')
+    layers = [list(g[1]) for g in groupby(int_rxns, sort_func)]
+
+    print(f'Reactions found: {len(rxns)}, integer: {len(int_rxns)}')
+    for i in range(len(layers)):
+        print(f'Layer {i+1}:')
+        print(*(gen.reaction_to_str(*rxn) for rxn in layers[i]), sep='\n')
